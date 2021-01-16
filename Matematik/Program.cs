36ab18f5780142c08f07c2d9ec4291f41b1c19c5
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2021-01-16 23:47:39 +0300

    16 Ocak 2021 Engin Demiroğ Ders Yaptıklarım

---
+++ Matematik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(5, 5000000);
+
+            dortIslem.Topla(5, 100);
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2};
+
+            //type-safe -- tip güvenli
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------------------");
+            }
+
+            Console.WriteLine("---------------------------METOTLAR-------------------------");
+            //instance -- örnek
+            //encapsulation
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12, 10);
+            sepetManager.Ekle2("Çilek", "Çilek Gibi Çilek", 45, 9);
+            sepetManager.Ekle2("Muz", "Muz Gibi Muzs", 80, 8);
+        }
+    }
+}
