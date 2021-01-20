072249493b1c664b081ad15412a3ad4bc8c111a1
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2021-01-20 23:59:10 +0300

    20 Ocak 2021

---
+++ genericsİntro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace genericsİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Ertuğrul");
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Mehmet");
+            Console.WriteLine(isimler.Length);
+
+            foreach (var item in isimler.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
